dce9e67f84b7ff7ddb3845438562d1c5873e17df
Michael Stapelberg <michael@stapelberg.de>
2010-09-17 02:25:06 +0200

    more compact initialization of the ethernet packet

--- busmaster/main.c
+++ busmaster/main.c
@@ -39,7 +39,40 @@
 #endif
 
 uint16_t uip_len = 0;
-uint8_t uip_buf[UIP_BUFSIZE+2];
+uint8_t uip_buf[UIP_BUFSIZE+2] =
+{
+    /* mac destination: IPv6 multicast */
+    0x33, 0x33, 0x00, 0xb5, 0x00, 0x01,
+    /* mac source: locally administered (02), hausbus (b5) */
+    0x02, 0xb5, 0x00, 0x00, 0x00, 0x01,
+    /* ethertype (IP) */
+    0x86, 0xdd,
+
+    /* ethernet payload */
+    0x60,
+    /* flowlabel */
+    0x00, 0x00, 0x00,
+    /* total length (will be filled in later) */
+    0x00, 0x00,
+    //uip_buf[19] = 8 /* udp header */ + payload_len;
+
+    /* next header: udp */
+    0x11,
+    /* hop limit */
+    0x02,
+    /* source: fe80::b5:ff:fe00:0001 */
+    0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x00, 0xff, 0xfe, 0x00, 0x00, 0x01,
+    /* destination: ff02::b5:1 */
+    0xff, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xb5, 0x00, 0x01,
+
+    /* data */
+    /* UDP */
+    0x02, /* source port low */
+    0x02, /* source port high */
+    0xA4, /* dest port low */
+    0x0F, /* dest port high */
+    0 /* data length high */
+};
 
 bool gotmsg = true;
 
@@ -134,85 +167,6 @@ int main(int argc, char *argv[]) {
         init_enc28j60();
 
         DBG("Initialized ENC28J60\r\n");
-
-        /* paket aufbereiten und schicken */
-        /* mac destination: IPv6 multicast */
-        uip_buf[0] = 0x33;
-        uip_buf[1] = 0x33;
-        uip_buf[2] = 0x00;
-        uip_buf[3] = 0xb5;
-        uip_buf[4] = 0x00;
-        uip_buf[5] = 0x01;
-        /* mac source */
-        uip_buf[6] = 0xAC;
-        uip_buf[7] = 0xDE;
-        uip_buf[8] = 0x48;
-        uip_buf[9] = 0xFD;
-        uip_buf[10] = 0x0F;
-        uip_buf[11] = 0xD0;
-        /* ethertype (IP) */
-        uip_buf[12] = 0x86;
-        uip_buf[13] = 0xdd;
-
-        /* ethernet payload */
-        uip_buf[14] = 0x60; /* ipv6, traffic class 0 */
-        /* flowlabel */
-        uip_buf[15] = 0x00;
-        uip_buf[16] = 0x00;
-        uip_buf[17] = 0x00;
-        /* total length: */
-        uip_buf[18] = 0x00;
-        //uip_buf[19] = 8 /* udp header */ + payload_len;
-
-        /* next header: udp */
-        uip_buf[20] = 0x11;
-        /* hop limit */
-        uip_buf[21] = 0x02;
-        /* source: fe80::aede:48ff:fefd:0fd0 */
-        uip_buf[22] = 0xfe;
-        uip_buf[23] = 0x80;
-        uip_buf[24] = 0x00;
-        uip_buf[25] = 0x00;
-        uip_buf[26] = 0x00;
-        uip_buf[27] = 0x00;
-        uip_buf[28] = 0x00;
-        uip_buf[29] = 0x00;
-        uip_buf[30] = 0xae;
-        uip_buf[31] = 0xde;
-        uip_buf[32] = 0x48;
-        uip_buf[33] = 0xff;
-        uip_buf[34] = 0xfe;
-        uip_buf[35] = 0xfd;
-        uip_buf[36] = 0x0f;
-        uip_buf[37] = 0xd0;
-        /* destination: ff02::b5:1 */
-        uip_buf[38] = 0xff;
-        uip_buf[39] = 0x02;
-
-        uip_buf[40] = 0x00;
-        uip_buf[41] = 0x00;
-        uip_buf[42] = 0x00;
-        uip_buf[43] = 0x00;
-
-        uip_buf[44] = 0x00;
-        uip_buf[45] = 0x00;
-        uip_buf[46] = 0x00;
-        uip_buf[47] = 0x00;
-
-        uip_buf[48] = 0x00;
-        uip_buf[49] = 0x00;
-        uip_buf[50] = 0x00;
-        uip_buf[51] = 0xb5;
-        uip_buf[52] = 0x00;
-        uip_buf[53] = 0x01;
-
-        /* data */
-        /* UDP */
-        uip_buf[54] = 0x02; /* source port low */
-        uip_buf[55] = 0x02; /* source port high */
-        uip_buf[56] = 0x02; /* dest port low */
-        uip_buf[57] = 0x02; /* dest port high */
-        uip_buf[58] = 0; /* data length high */
 
         char buf[16] = "serial:       X\n";
             for (;;) {
