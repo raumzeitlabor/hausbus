496e5ab50719c9c15c668e0302e1a7fe35ba90f3
Michael Stapelberg <michael@stapelberg.de>
2010-09-17 23:16:59 +0200

    dump.rb: join group 1

--- application/dump.rb
+++ application/dump.rb
@@ -4,7 +4,7 @@ require 'bus'
 
 bus = Hausbus.new
 bus.set_interface "eth0"
-bus.join_group 38, do |msg, sender|
+bus.join_group 1, do |msg, sender|
   puts "MSG from #{sender} len #{msg.size}" 
   for c in 0..msg.size-1
     puts "c #{c} = #{msg[c]}, #{msg[c].ord.to_s(16)}"
