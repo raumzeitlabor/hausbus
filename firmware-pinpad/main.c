71f1dacfdc3abaded94f7fce88dc002eace238ab
Michael Stapelberg <michael@stapelberg.de>
2011-02-09 08:19:44 +0100

    implement old functionality with fixed pin

--- firmware-pinpad/main.c
+++ firmware-pinpad/main.c
@@ -11,20 +11,100 @@
 #include <util/delay.h>
 #include <stdbool.h>
 #include <stdint.h>
+#define BAUD 9600
+#include <util/setbaud.h>
+
 
 #include "bus.h"
 #include "uart2.h"
 
-#define DEBUG
+//#define DEBUG
 #ifdef DEBUG
   #define DBG(x) uart2_puts_P(PSTR(x))
 #else
   #define DBG(x) (void)0
 #endif
 
+/* seite 84 */
+static char pin[11];
+static uint8_t pincnt = 0;
+static volatile char serbuf[64];
+static volatile uint8_t sercnt = 0;
+
 static uint8_t packetcnt = 0;
 static uint8_t lbuffer[32];
 static uint8_t rbuffer[32];
+
+ISR(USART1_RX_vect) {
+    uint8_t byte;
+    uint8_t usr;
+
+    usr = UCSR1A;
+    byte = UDR1;
+
+    if (serbuf[8] == '$')
+        return;
+
+    if (sercnt == 0 && byte != '^')
+        return;
+
+    if (sercnt == 8 && byte != '$')
+        return;
+
+    serbuf[sercnt] = byte;
+    sercnt++;
+
+    if (sercnt == 9)
+        sercnt = 0;
+}
+
+#if 0
+static void uart2_puts(const char *str) {
+    int c;
+
+    for (c = 0; c < strlen(str); c++) {
+        while ( !( UCSR1A & (1<<UDRE1)) );
+        UDR1 = (unsigned char)str[c];
+    }
+}
+#endif
+
+static void handle_command(const char *buffer) {
+    if (strncmp(buffer, "^PAD ", strlen("^PAD ")) == 0) {
+        char c = buffer[5];
+        if (pincnt < 10) {
+            pin[pincnt] = c;
+            pincnt++;
+        }
+        uart2_puts("^LED 2 1$\n");
+        uart2_puts("^BEEP 1 $\n");
+        char *str;
+        if (pincnt == 7 && strncmp(pin, "777#", 5) == 0) {
+            uart2_puts("^LED 2 2$");
+            uart2_puts("^BEEP 2 $\n");
+            pincnt = 0;
+            memset(pin, '\0', sizeof(pin));
+            PORTA &= ~(1 << PA2);
+            _delay_ms(500);
+            PORTA |= (1 << PA2);
+        } else if (pincnt == 4 && strncmp(pin, "666#", 5) == 0) {
+            uart2_puts("^LED 2 2$");
+            uart2_puts("^BEEP 2 $\n");
+            pincnt = 0;
+            memset(pin, '\0', sizeof(pin));
+            PORTA &= ~(1 << PA3);
+            _delay_ms(500);
+            PORTA |= (1 << PA3);
+        } else if (c == '#') {
+            uart2_puts("^LED 1 2$^BEEP 2 $");
+            //uart2_puts("^BEEP 2 $\n");
+            pincnt = 0;
+            memset(pin, '\0', sizeof(pin));
+        }
+
+    }
+}
+
 
 /*
  * Sends the packet in the given buffer. Basically a wrapper around
@@ -41,6 +121,7 @@ static void send_reply(uint8_t *buffer) {
 }
 
 int main(int argc, char *argv[]) {
+    char bufcopy[10];
     uint8_t status;
 
     /* enable LED so that the user knows the controller is active */
@@ -52,14 +133,39 @@ int main(int argc, char *argv[]) {
     WDTCSR &= ~(1 << WDE);
     wdt_disable();
 
+    /* set pins for the schließer */
+    DDRA = (1 << PA2) | (1 << PA3);
+    PORTA = (1 << PA2) | (1 << PA3);
+
+    /* init serial line to the pinpad frontend */
+    UBRR1H = UBRRH_VALUE;
+    UBRR1L = UBRRL_VALUE;
+    UCSR1B = (1 << RXCIE1) | (1 << RXEN1) | (1 << TXEN1);
+
+    UCSR1C = (1<<UCSZ10) | (1<<UCSZ11);
+
     net_init();
-    uart2_init();
 
     sei();
 
+    _delay_ms(500);
+    PORTC = 0;
+    _delay_ms(500);
+    PORTC = (1 << PC7);
+
     DBG("Pinpad firmware booted.\r\n\r\n");
+    char *str = "^AEEP 1 $\n";
+    int c;
     while (1) {
-        _delay_ms(100);
+
+        if (serbuf[8] == '$') {
+            strncpy(bufcopy, serbuf, sizeof(bufcopy));
+            serbuf[8] = '\0';
+
+            handle_command(bufcopy);
+        }
+
+        //_delay_ms(100);
         status = bus_status();
 
         if (status == BUS_STATUS_IDLE) {
